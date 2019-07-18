4a90797768d6b261a1611b7a319c95d9eca6fea1
lubingming <lubingming@ky-tech.com.cn>
2019-07-18 20:40:37 +0800

    add springcloud-config

---
+++ springcloud-config-client/src/main/java/com/lbm/springcloudconfigclient/SpringcloudConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.lbm.springcloudconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringcloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigClientApplication.class, args);
+    }
+
+}
